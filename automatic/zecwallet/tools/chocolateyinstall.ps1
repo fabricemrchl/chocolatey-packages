1f64e8b501d2d53fb45c9b22e6053a00af24a626
Le_Bleu <fabrice@fmarchal.fr>
2020-01-06 21:42:30 +0100

    add zecwallet package

---
+++ automatic/zecwallet/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi' #only one of these: exe, msi, msu
+  url64bit      = 'https://github.com/ZcashFoundation/zecwallet/releases/download/v0.8.1/Windows-installer-zecwallet-v0.8.1.msi'
+
+  softwareName  = 'zecwallet-lite*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+  checksum64    = '2546A04C5429FDFBF477AC540E29F0EE01C0DFEFEE045D35E3A191410AB5A161'
+  checksumType64= 'sha256' #default is checksumType
+
+  # MSI
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+
+}
+
+Install-ChocolateyPackage @packageArgs 
