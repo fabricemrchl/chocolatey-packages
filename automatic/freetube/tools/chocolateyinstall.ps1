9e550a51ea23ac9e7ad71ae43ae181c72c89e61a
Fabrice Marchal <fabrice@fmarchal.fr>
2020-05-27 20:35:37 +0200

    freetube : add package

---
+++ automatic/freetube/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' #only one of these: exe, msi, msu
+  url64bit      = 'https://github.com/FreeTubeApp/FreeTube/releases/download/v0.7.2-beta/FreeTube.Setup.0.7.2.exe'
+
+  softwareName  = 'FreeTube*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+  checksum64    = '106bf74fabfd40d1b6cfb4fe2d8de555b9ae1857a9595856c15b17352957ded5'
+  checksumType64= 'sha256' #default is checksumType
+
+  # MSI
+  silentArgs    = "/S" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+
+}
+
+Install-ChocolateyPackage @packageArgs 
---
+++ automatic/freetube/update.ps1
@@ -0,0 +1,29 @@
+import-module au
+
+$release_url = 'https://github.com/FreeTubeApp/FreeTube/releases'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(?i)(^\s*checksum64\s*=\s*)'(.*)'"   = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*url64bit\s*=\s*)'(.*)'"   = "`$1'$($Latest.Url64)'"
+            
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $release_url -UseBasicParsing
+
+    $re = '\.exe$'
+    $url = $download_page.Links | ? href -match $re | select -first 1 -expand href | % { 'https://github.com' + $_ }
+
+    $version = [regex]::match($url,'\/v?(\d[\.\d*]*(\-beta)?)\/').Groups[1].Value
+
+    @{
+        URL64 = $url
+        Version = $version
+    }
+}
+
+update -NoCheckUrl -ChecksumFor 64
