c8b887f4e8b15e9812a1fe53a138f171efbec388
Le_Bleu <fabrice@marchal.fr>
2020-10-20 00:05:55 +0000

    AU: 2 updated - zecwallet zecwallet-lite [skip ci] https://gist.github.com/cf6b5da44848b940783683b6883a8bef/ebbe4b6752c7221c14e241fa82d1c8ef63e671c7

--- automatic/zecwallet-lite/tools/chocolateyinstall.ps1
+++ automatic/zecwallet-lite/tools/chocolateyinstall.ps1
@@ -8,11 +8,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'msi' #only one of these: exe, msi, msu
-  url64bit      = 'https://github.com/adityapk00/zecwallet-lite/releases/download/v1.2.2/Zecwallet.Lite.1.2.2.msi'
+  url64bit      = 'https://github.com/adityapk00/zecwallet-lite/releases/download/v1.3.0/Zecwallet.Lite.1.3.0.msi'
 
   softwareName  = 'zecwallet-lite*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
-  checksum64    = 'e0d0683fbc0e81712099e2daa6b3839d0da0a74630994c9079b5cde7af9c8e64'
+  checksum64    = '9e4d6415edca9453b14eb0cddd831349c21dc9a90bf77661cf76d432e34c2377'
   checksumType64= 'sha256' #default is checksumType
 
   # MSI
--- automatic/zecwallet/tools/chocolateyinstall.ps1
+++ automatic/zecwallet/tools/chocolateyinstall.ps1
@@ -8,11 +8,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'msi' #only one of these: exe, msi, msu
-  url64bit      = 'https://github.com/ZcashFoundation/zecwallet/releases/download/v0.9.18/Zecwallet.Fullnode.0.9.18.msi'
+  url64bit      = 'https://github.com/ZcashFoundation/zecwallet/releases/download/v0.9.19/Zecwallet.Fullnode.0.9.19.msi'
 
   softwareName  = 'Zecwallet-Fullnode*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
-  checksum64    = 'cee9856f75f21205072b3b43e43f6bf38ed485451e6b5d98e78dbb33e7dd05a4'
+  checksum64    = '44d4d773be3257a7d2d33969eb2242beab734617e704d2ebe379812f0f0b4192'
   checksumType64= 'sha256' #default is checksumType
 
   # MSI
