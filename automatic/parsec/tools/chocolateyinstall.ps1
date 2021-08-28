dda23358c1caa63913a4c8b03d1081068cab9d23
Fabrice Marchal <fabrice@fmarchal.fr>
2021-08-28 15:18:19 +0200

    Parsec : add parameters

--- automatic/parsec/tools/chocolateyinstall.ps1
+++ automatic/parsec/tools/chocolateyinstall.ps1
@@ -2,7 +2,15 @@
 $ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
+$pp = Get-PackageParameters
 
+$sa = " /silent "
+
+# Prevent clean user settings by default, if you want to clean settings at each update add parameter /CleanUser
+$sa += if (-Not $pp['CleanUser']) { " /nocleanuser " }
+
+#Use the same Parsec account for all users on this computer
+$sa += if ($pp['Shared']) { " /shared " }
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -12,13 +20,13 @@ $packageArgs = @{
   
   url            = "https://builds.parsecgaming.com/package/parsec-windows32.exe"
   url64bit       = "https://builds.parsecgaming.com/package/parsec-windows.exe"
-  checksum       = 'b2bf9a7f566fa23c4d53725c9247229b0f86c087554c508b138440e2376385a8'
+  checksum       = 'cc05a5a691640a2b49bc8e95ec3369ceae371ba9e6fcf6dcf23b41623cee68d8'
   checksumType   = 'sha256'
-  checksum64     = '57fb335bcf2900db0741748975608bdc4417fa20679478d343b28ad392dfbc4b'
+  checksum64     = '7de46de443b7d3908280bd2469829b9904853efccdacd8c0de5cf9ea8ce898e8'
   checksumType64 = 'sha256'
 
   # MSI
-  silentArgs    = "/S"
+  silentArgs    = "$sa /S"
   validExitCodes= @(0, 3010, 1641)
 
 }
