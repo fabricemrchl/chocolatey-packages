714d0ad6fc1002f6b53501fa20b270234c6138b3
Le_Bleu <fabrice@marchal.fr>
2021-09-05 00:05:57 +0000

    AU: 1 updated - freetube [skip ci] https://gist.github.com/cf6b5da44848b940783683b6883a8bef/2317170459fdf550ca54894d282680579912dd43

--- automatic/freetube/tools/chocolateyinstall.ps1
+++ automatic/freetube/tools/chocolateyinstall.ps1
@@ -8,11 +8,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe' #only one of these: exe, msi, msu
-  url64bit      = 'https://github.com/FreeTubeApp/FreeTube/releases/download/v0.13.1-beta/freetube-0.13.1-setup-x64.exe'
+  url64bit      = 'https://github.com/FreeTubeApp/FreeTube/releases/download/v0.14.0-beta/freetube-0.14.0-setup-x64.exe'
 
   softwareName  = 'FreeTube*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
-  checksum64    = '1c9fa34613533f1578c167f588ecbaf9293e9b3d15ecb088134b211a5be6fc98'
+  checksum64    = 'b767f421917de075a38381f6495e5bbb5ba058b25d1fa3145ffa714919743f71'
   checksumType64= 'sha256' #default is checksumType
 
   # MSI
