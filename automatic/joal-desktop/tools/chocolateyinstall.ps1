763da2ee4b2f86169a76f092f5d878ed753f617d
Le_Bleu <fabrice@marchal.fr>
2021-11-17 00:06:05 +0000

    AU: 1 updated - joal-desktop [skip ci] https://gist.github.com/cf6b5da44848b940783683b6883a8bef/d254f82c1f493d82f3ea71094063998039d529ec

--- automatic/joal-desktop/tools/chocolateyinstall.ps1
+++ automatic/joal-desktop/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url           = 'https://github.com/anthonyraymond/joal-desktop/releases/download/v2.0.6/JoalDesktop-2.0.6-win.exe'
+  url           = 'https://github.com/anthonyraymond/joal-desktop/releases/download/v2.0.8/JoalDesktop-2.0.8-win-x64.exe'
   
   softwareName  = 'joal-desktop*'
 
-  checksum      = 'dc2e9f5b635a44b94944938d292e544c539386b51ee15f516bc40bd6dc62740b'
+  checksum      = '3867489f580ba4ff3ff0a3c64fb30bb6cabdde5752f894b77b236b923849f9f5'
   checksumType  = 'sha256'
   silentArgs   = '/S' 
   validExitCodes= @(0)
