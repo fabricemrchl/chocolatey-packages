162da802b651f086f1b6acd091e2507f420ab44d
Le_Bleu <31774851+fabricemrchl@users.noreply.github.com>
2021-12-04 18:34:02 +0100

    cleanup default comment

--- automatic/fpsmonitor/tools/chocolateyinstall.ps1
+++ automatic/fpsmonitor/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿
-$ErrorActionPreference = 'Stop'; # stop on all errors
+$ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  softwareName  = 'FPS Monitor*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  softwareName  = 'FPS Monitor*'
   unzipLocation = $toolsDir
-  fileType      = 'exe' #only one of these: exe, msi, msu
+  fileType      = 'exe' 
   
   url            = 'https://www.fpsmon.com/bin/fpsmon-5400.exe'
   checksum       = '4bfd12ed12c94e6d4c8fb816fb99023b18eafe785d84dd04f56a104f85c46d6a'
