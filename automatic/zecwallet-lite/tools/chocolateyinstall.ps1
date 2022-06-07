42cc4a8b4451b9c748807ec5522e924d4029c0e7
Le_Bleu <fabrice@marchal.fr>
2022-06-07 08:06:42 +0000

    AU: 1 updated - zecwallet-lite [skip ci] https://gist.github.com/cf6b5da44848b940783683b6883a8bef/6a9253f426e3b46e77b81bce9d326d8005c99e04

--- automatic/zecwallet-lite/tools/chocolateyinstall.ps1
+++ automatic/zecwallet-lite/tools/chocolateyinstall.ps1
@@ -8,11 +8,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'msi' #only one of these: exe, msi, msu
-  url64bit      = 'https://github.com/adityapk00/zecwallet-lite/releases/download/v1.7.10/Zecwallet.Lite.1.7.10.msi'
+  url64bit      = 'https://github.com/adityapk00/zecwallet-lite/releases/download/v1.7.13/Zecwallet.Lite.1.7.13.msi'
 
   softwareName  = 'zecwallet-lite*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
-  checksum64    = '801182aa289bd643c8756263c6bf4fa3ef3ed911f700c4be86e8d282f083a80c'
+  checksum64    = '1782ebaf255f539a07899e766da0e3e1361d177a0ea2b32c1f88793f91e4b523'
   checksumType64= 'sha256' #default is checksumType
 
   # MSI
