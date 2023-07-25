df37b4ea8950c04d8649909cb42d7a310d1c3640
Le_Bleu <fabrice@marchal.fr>
2023-07-25 16:06:37 +0000

    AU: 1 updated - parsec [skip ci] https://gist.github.com/fabricemrchl/cf6b5da44848b940783683b6883a8bef/475777224c0ba6812ee8d1647e76158d80080e0e

--- automatic/parsec/tools/chocolateyinstall.ps1
+++ automatic/parsec/tools/chocolateyinstall.ps1
@@ -20,9 +20,9 @@ $packageArgs = @{
   
   url            = "https://builds.parsecgaming.com/package/parsec-windows32.exe"
   url64bit       = "https://builds.parsecgaming.com/package/parsec-windows.exe"
-  checksum       = '7426b11cd0032378bd629151302470838cd733fb22cda5d89afb91422b84af08'
+  checksum       = '282cb208cb6069d5931d3e7a16b85e41ff08ad4ddf72191b29cfe35eeac77580'
   checksumType   = 'sha256'
-  checksum64     = 'e17c059a2ec3153241f4cddf8081f19e83af890cb9126f3e1528474c29610786'
+  checksum64     = '5f70432c215ab7dd4f53882294779cfd5590268692aa0edae0be1cf274ca146f'
   checksumType64 = 'sha256'
 
   # MSI
