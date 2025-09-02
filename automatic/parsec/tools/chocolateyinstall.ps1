753fb9a4b91d5f0619d66ff223da069a469a0989
Léo El Amri <leo@superlel.me>
2025-09-02 21:13:15 +0200

    Update Parsec updater and installation script
    There are no 32bits version anymore

--- automatic/parsec/tools/chocolateyinstall.ps1
+++ automatic/parsec/tools/chocolateyinstall.ps1
@@ -18,11 +18,8 @@ $packageArgs = @{
   unzipLocation = $toolsDir
   fileType      = 'exe' #only one of these: exe, msi, msu
   
-  url            = "https://builds.parsecgaming.com/package/parsec-windows32.exe"
   url64bit       = "https://builds.parsecgaming.com/package/parsec-windows.exe"
-  checksum       = 'f6250ee5d02cbc50ee0128b9b0eecfdfb3b13cabf176777958b6971b6ed988d1'
-  checksumType   = 'sha256'
-  checksum64     = 'ae5b66322e5a7c26ad21ccc556bdc1618796166565d2939142c5aa3d76c38ace'
+  checksum64     = 'f0edc12c9f612507371727af54993bb052c6e52857b3b025acbbd720d3ef724e'
   checksumType64 = 'sha256'
 
   # MSI
